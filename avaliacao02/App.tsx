e6edc397f29b0e96d540724cf5b41fe3ebf60e04
mayaraduartez <mayaraduart321@gmail.com>
2023-02-17 16:51:21 -0300

    avaliacao pronta

---
+++ avaliacao02/App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import Registro from './screens/Registro'
+import estiloAuthentication from './estiloAuthentication';
+import Profile from './screens/Profile';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="Registro" component={Registro} />
+        <Stack.Screen options={{ headerShown: false }} name="Profile" component={Profile} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
---
+++ avaliacao02/model/Serie.tsx
@@ -0,0 +1,45 @@
+﻿export class Serie {
+    public id: string;
+    public titulo: string;
+    public genero: string;
+    public temporadas: number;
+    public ano: number;
+    public classificacao: string;
+    
+    constructor(obj?: Partial<Serie>) {
+        if (obj) {
+            this.id = obj.id
+            this.titulo = obj.titulo
+            this.genero = obj.genero
+            this.temporadas = obj.temporadas
+            this.ano = obj.ano
+            this.classificacao = obj.classificacao
+         }
+    }
+
+    toFirestore() {
+        const series =  {
+                    id : this.id,
+                    titulo : this.titulo,
+                    genero : this.genero,
+                    temporadas : this.temporadas,
+                    ano : this.ano,
+                    classificacao : this.classificacao
+         }   
+     
+         return series
+    }
+
+       
+    toString() {
+        const Objeto = `{
+            "id": "${this.id}",
+            "titulo": "${this.titulo}",
+            "genero": "${this.genero}",
+            "temporadas": "${this.temporadas}",
+            "ano": "${this.ano}",
+            "classificacao": "${this.classificacao}"
+        }`
+        return Objeto
+    }
+};
